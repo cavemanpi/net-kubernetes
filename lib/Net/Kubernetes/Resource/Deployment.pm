3dce941a1f2f5a53b933ccb29841d700a8102530
Kevin Johnson <kcavemanj@gmail.com>
2017-07-06 17:04:00 -0400

    Add bare bones class setup for Deployments.

---
+++ lib/Net/Kubernetes/Resource/Deployment.pm
@@ -0,0 +1,10 @@
+package Net::Kubernetes::Resource::Deployment;
+
+use Moose;
+
+extends 'Net::Kubernetes::Resource';
+with 'Net::Kubernetes::Resource::Role::State';
+with 'Net::Kubernetes::Resource::Role::Spec';
+with 'Net::Kubernetes::Resource::Role::HasPods';
+
+return 1;
