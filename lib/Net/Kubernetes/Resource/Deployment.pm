84f488976edd6220465b60ec72f86b6a683e2345
Kevin Johnson <kcavemanj@gmail.com>
2017-07-10 13:13:57 -0400

    Adds a method to scale a deployment.

--- lib/Net/Kubernetes/Resource/Deployment.pm
+++ lib/Net/Kubernetes/Resource/Deployment.pm
@@ -7,4 +7,20 @@ with 'Net::Kubernetes::Resource::Role::State';
 with 'Net::Kubernetes::Resource::Role::Spec';
 with 'Net::Kubernetes::Resource::Role::HasPods';
 
+sub scale {
+    my($self, $replicas, $timeout) = @_;
+    $timeout ||= 5;
+    $self->spec->{replicas} = $replicas;
+    $self->update;
+    my $st = time;
+    while((time - $st) < $timeout){
+        my $pods = $self->get_pods;
+        if(scalar(@$pods) == $replicas){
+            return "scaled";
+        }
+        sleep(0.3);
+    }
+    return 0;
+}
+
 return 1;
